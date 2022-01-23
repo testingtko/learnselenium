aac204a39595fc8b6cb9059d8520e53a1e8709ec
testingtko <tkotk13071991@gmail.com>
2022-01-23 21:03:12 +0530

    my first commit

---
+++ Automation/src/Assignment1.java
@@ -0,0 +1,23 @@
+public class Assignment1 {
+	
+	int a;
+	
+	public void deepak() {
+		
+		System.out.println("test");
+	}
+	
+	public static void main(String[] args) {
+		
+		Assignment1 as=new Assignment1();
+		
+		as.deepak();
+		
+		as.a=21;
+		
+		System.out.println("i am the main method");
+	}
+
+}
+
+
